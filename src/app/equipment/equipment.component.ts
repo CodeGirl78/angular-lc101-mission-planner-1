19dcefe6ce2a2c52f45a4ad66f40fa9b9237d04c
Melinda <codegirl78@gmail.com>
2021-06-25 23:23:02 -0500

    Attempted Bonus Mission Parts 2-7, but was unsuccessful.  Will try again

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -5,11 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './equipment.component.html',
   styleUrls: ['./equipment.component.css']
 })
+
 export class EquipmentComponent implements OnInit {
+  equipmentItems: string[] = ['Command Module', 'Space Camera', 'Food', 'Oxygen Tanks'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /**Bonus Mission Part 2 */
+  add(equipmentItems: "string": boolean) {
+    this.equipmentItems.push({equipmentItems: this.equipmentItems});
+  }
+
+  /**Bonus Mission Part 4 */
+  remove(equipmentItems: "string") {
+    let index = this.equipmentItems.indexOf(equipmentItems);
+    this.equipmentItems.splice(index, 1);
+  }
+
+  /**Bonus Mission Part 3 */
+  edit(equipmentItems: "string") {
+    this.equipmentItemsBeingEdited = this.equipmentItems;
+  }
+  
+  /**Bonus Mission */
+  save(equipmentItems: "string") {
+    equipmentItems['equipmentItems'] = equipmentItems;
+    this.equipmentItemsBeingEdited = null;
   }
 
 }

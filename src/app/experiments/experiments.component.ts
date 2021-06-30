8755a140a6dc85d019298498c8f4bd2afe26e7a3
Melinda <codegirl78@gmail.com>
2021-06-29 19:58:56 -0500

    Added div classes for header, crew, equipment, and experiments to app.component.html

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments = [{name:"Mars soil sample", editingExp: false}, {name:"Plant growth in habitat", editingExp: false},
+  {name:"Human bone density", editingExp: false}];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  allExp(){
+    let names = [];
+    for(let i=0; i<this.experiments.length; i++){
+      names.push(this.experiments[i]["name"])
+    } return names;
+  }
+
+  /**Bonus Mission Part 5 */
+  addEquip(exp){
+    let object = {name: "", editingExp: false};
+    object.name = exp;
+    if(!this.allExp().includes(exp)){
+      this.experiments.push(object);
+    }
+  }
+
+  /**Bonus Mission Part 7 */
+  removeEquip(exp){
+    this.experiments.splice(this.experiments.indexOf(exp), 1)
+  }
+
+  /**Bonus Mission Part 6 */
+  updateEquip(newName:string, item) {
+    this.experiments[this.experiments.indexOf(item)]["name"] = newName;
+    item.editingExp = false;
   }
 
 }

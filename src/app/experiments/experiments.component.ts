04183941e799a26d7914dcefd63086244fd578b3
Melinda <codegirl78@gmail.com>
2021-07-06 20:25:54 -0500

    Working on Bonus Mission

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-experiments',
-  templateUrl: './experiments.component.html',
-  styleUrls: ['./experiments.component.css']
-})
-export class ExperimentsComponent implements OnInit {
-   experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density changes', 'Water droplet podcast for grades K-5', 'Satellite launch'];
-
-   constructor() { }
-
-   ngOnInit() { }
-
-}
+	
+	@Component({
+	  selector: 'app-experiments',
+	  templateUrl: './experiments.component.html',
+	  styleUrls: ['./experiments.component.css']
+	})
+	export class ExperimentsComponent implements OnInit {
+	   experiments: object[] = [{name:'Moon soil sample', selec:false}, {name:'Plant growth in orbit', selec:false}, 
+	                            {name:'Human bone density changes', selec:false}, {name:'Water droplet podcast for grades K-5', selec:false}, 
+	                            {name:'Satellite launch', selec:false}];
+	   chosenExperiments: string[] = [];
+	   experimentStat:string = "FULL";
+	   constructor() { }
+	
+	   ngOnInit() { }
+	
+	   addExp = (item) => {
+	    if(!this.chosenExperiments.includes(item)){
+	      if(this.chosenExperiments.length < 3){
+	        this.chosenExperiments.push(item);
+	        item.selec = true;
+	      }
+	    } else{
+	      this.chosenExperiments.splice(this.chosenExperiments.indexOf(item), 1);
+	      item.selec = false;
+	    }
+	  }
+	
+	}
